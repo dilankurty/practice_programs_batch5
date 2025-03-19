90842464ed6e223ab147d688a4d3934ab3e4a98e
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-19 13:10:54 +0800

    used len function but not working

--- number_of_words.py
+++ number_of_words.py
@@ -1,3 +1,5 @@
 # Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
 statement = input("Enter a statement: ")
+
+print(len(statement))
