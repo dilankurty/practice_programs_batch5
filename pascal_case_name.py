87bde006e3f3b494f018424f79fb3154ca492a67
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-19 13:28:56 +0800

    used title and strip method but not working

--- pascal_case_name.py
+++ pascal_case_name.py
@@ -1,3 +1,5 @@
 # Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 name = input("Enter your full name: ")
+
+print(name.title().strip())
