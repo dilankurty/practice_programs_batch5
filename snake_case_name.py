59b8efb0084e4c7fb53e4bc286239537334321dc
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-19 13:36:48 +0800

    use lower method

--- snake_case_name.py
+++ snake_case_name.py
@@ -1,3 +1,5 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 
 name = input("Enter your full name: ")
+
+print(name.lower())
